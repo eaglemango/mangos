4df02ece10ad76201bd0c0cda36bcc47c971f1c7
eaglemango <eaglemango@gmail.com>
2020-10-12 21:38:32 +0300

    Add memchr function

--- libc/include/string.h
+++ libc/include/string.h
@@ -5,6 +5,7 @@
 
 void* memcpy(void* dest_ptr, const void* src_ptr, size_t size);
 void* memset(void* ptr, int value, size_t size);
+void* memchr(const void* ptr, int symbol, size_t size);
 
 size_t strlen(const char* str);
 
---
+++ libc/string/memchr.c
@@ -0,0 +1,11 @@
+#include "string.h"
+
+void* memchr(const void* ptr, int symbol, size_t size) {
+    for (size_t i = 0; i < size; ++i) {
+        if (((char*) ptr)[i] == (char) symbol) {
+            return ptr;
+        }
+    }
+
+    return NULL;
+}
