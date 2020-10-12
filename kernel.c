bac6426f1130d8ed6b75fb28bc570beccaafab5d
eaglemango <eaglemango@gmail.com>
2020-10-12 18:37:45 +0300

    Fix increments

--- kernel.c
+++ kernel.c
@@ -34,7 +34,7 @@ static inline uint16_t vga_entry(unsigned char uc, uint8_t color) {
 size_t strlen(const char* str) {
 	size_t len = 0;
 	while (str[len]) {
-		len++;
+		++len;
     }
 	return len;
 }
@@ -51,8 +51,8 @@ void terminal_initialize(void) {
 	terminal_column = 0;
 	uint8_t color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
 	terminal_buffer = (uint16_t*) 0xB8000;
-	for (size_t y = 0; y < VGA_HEIGHT; y++) {
-		for (size_t x = 0; x < VGA_WIDTH; x++) {
+	for (size_t y = 0; y < VGA_HEIGHT; ++y) {
+		for (size_t x = 0; x < VGA_WIDTH; ++x) {
 			const size_t index = y * VGA_WIDTH + x;
 			terminal_buffer[index] = vga_entry(' ', color);
 		}
@@ -67,7 +67,7 @@ void terminal_putentryat(char c, uint8_t color, size_t x, size_t y) {
 void terminal_putchar_color(char c, uint8_t color) {
     switch (c) {
     case '\n':
-        terminal_row++;
+        ++terminal_row;
         terminal_column = 0;
         if (terminal_row == VGA_HEIGHT) {
             terminal_row = 0;
@@ -76,7 +76,7 @@ void terminal_putchar_color(char c, uint8_t color) {
 
     default:
         terminal_putentryat(c, color, terminal_column, terminal_row);
-        terminal_column++;
+        ++terminal_column;
         if (terminal_column == VGA_WIDTH) {
             terminal_column = 0;
             terminal_row++;
@@ -88,7 +88,7 @@ void terminal_putchar_color(char c, uint8_t color) {
 }
 
 void terminal_write(const char* data, size_t size, uint8_t color) {
-	for (size_t i = 0; i < size; i++) {
+	for (size_t i = 0; i < size; ++i) {
         terminal_putchar_color(data[i], color);
     }
 }
